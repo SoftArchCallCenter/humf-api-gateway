ac270d60c7a389479b5f320dde7b87279de2bbfa
Stickman008 <arm.supakorn@gmail.com>
2023-09-17 18:09:43 +0700

    Update app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,9 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
-import { RestaurantsModule } from './restaurants/restaurants.module';
 
 @Module({
-  imports: [UsersModule, AuthModule, RestaurantsModule],
+  imports: [UsersModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
