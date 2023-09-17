01cc416764511b31fb5869f77bd1f280d3609e97
ThanachatAim <Thanachat.aim@gmail.com>
2023-09-18 03:36:28 +0700

    delete unuse file

--- src/client/client.controller.ts
+++
@@ -1,7 +0,0 @@
-import { Controller } from '@nestjs/common';
-import { ClientService } from './client.service';
-
-@Controller()
-export class ClientController {
-  constructor(private readonly clientService: ClientService) {}
-}
