fb4dfb117befcc009b02f2dc0bc46a48d66dc934
Stickman008 <arm.supakorn@gmail.com>
2023-09-20 14:41:47 +0700

    Update auth handler

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { HttpService } from '@nestjs/axios';
-import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { HttpCode, HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { response } from 'express';
 import { Observable, catchError, firstValueFrom, map } from 'rxjs';
 import { LoginUserDto } from 'src/users/dto/login-user.dto';
 import { SignupUserDto } from 'src/users/dto/signup-user.dto';
@@ -13,17 +14,38 @@ export class AuthService {
         private readonly configService: ConfigService,
     ) {}
 
-    async signup(userData: SignupUserDto): Promise<Observable<any>> {
-        const authServiceUrl = this.configService.get<string>('USER_SERVICE_URL');
-        const signupUrl = `${authServiceUrl}/auth/signup`;
-        const response =  await firstValueFrom(this.httpService.post(signupUrl, userData));
-        return response.data;
+    async signup(userData: SignupUserDto): Promise<any> {
+        try {
+            const authServiceUrl = this.configService.get<string>('USER_SERVICE_URL');
+            const signupUrl = `${authServiceUrl}/auth/signup`;
+            const response = await firstValueFrom(this.httpService.post(signupUrl, userData));
+            
+            if (response.status === HttpStatus.CREATED) {
+                return response.data;
+            } else {
+                throw new HttpException('User signup failed', HttpStatus.BAD_REQUEST);
+            }
+        } catch (error) {
+            // Handle other errors such as network issues or timeouts
+            throw new HttpException('User signup failed', HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
-    async login(credentials: LoginUserDto): Promise<Observable<any>> {
-        const authServiceUrl = this.configService.get<string>('USER_SERVICE_URL');
-        const loginUrl = `${authServiceUrl}/auth/login`;
-        const response =  await firstValueFrom(this.httpService.post(loginUrl, credentials));
-        return response.data;
+    async login(credentials: LoginUserDto): Promise<any> {
+        try {
+            const authServiceUrl = this.configService.get<string>('USER_SERVICE_URL');
+            const loginUrl = `${authServiceUrl}/auth/login`;
+            const response = await firstValueFrom(this.httpService.post(loginUrl, credentials));
+            
+            if (response.status === HttpStatus.CREATED) {
+                return response.data;
+            } else {
+                // Handle unexpected status codes
+                throw new HttpException('Login failed', HttpStatus.UNAUTHORIZED);
+            }
+        } catch (error) {
+            // Handle other errors such as network issues or timeouts
+            throw new HttpException('Login failed', HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
