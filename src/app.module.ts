dbc28e099c28df72f34174c64af9d14103367600
ThanachatAim <Thanachat.aim@gmail.com>
2023-10-22 06:49:42 +0700

    add menu service

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,11 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { RestaurantModule } from './restaurant/restaurant.module';
+import { MenuModule } from './menu/menu.module';
 
 
 @Module({
-  imports: [UsersModule, AuthModule, RestaurantModule],
+  imports: [UsersModule, AuthModule, RestaurantModule, MenuModule],
   controllers: [AppController],
   providers: [AppService],
 })
