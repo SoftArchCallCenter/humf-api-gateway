a69f19d5a738a0e55c9c5773e1813c2c394b777c
Stickman008 <69195590+Stickman008@users.noreply.github.com>
2023-11-09 23:00:12 +0700

    Update 2

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,10 @@ dotenv.config();
 
 async function bootstrap() {
   const port = process.env.API_GATEWAY_PORT || 5000;
+  const host = process.env.API_GATEWAY_HOST || "127.0.0.1";
 
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(port);
+  await app.listen(port, host);
 }
 bootstrap();
