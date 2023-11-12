7085d3debf7198d7dd78a032e8c535199dbe3840
G10N(nick) <nick.supakorn@hotmail.com>
2023-11-12 14:06:48 +0700

    update user service path

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,8 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { ConfigService } from '@nestjs/config';
 import { HttpService } from '@nestjs/axios';
+import { AxiosError } from 'axios';
 import { Observable, catchError, firstValueFrom, map } from 'rxjs';
 
 @Injectable()
@@ -15,37 +16,61 @@ export class UsersService {
   async create(createUserDto: CreateUserDto) {
     const userServiceUrl = this.configService.get<string>('USER_SERVICE_URL');
     const creatUserUrl = `${userServiceUrl}/users`;
-    const response = await firstValueFrom(
-      this.httpService.post(creatUserUrl, createUserDto)
-    );
-    return response.data
+    return this.httpService
+      .post(creatUserUrl, createUserDto)
+      .pipe(map((resp) => resp.data))
+      .pipe(
+        catchError((error: AxiosError) => {
+          const errorMessage =
+            error.response?.data || 'An internal error occurred.';
+          throw new InternalServerErrorException(errorMessage);
+        }),
+      );
   }
 
   async findAll() {
     const userServiceUrl = this.configService.get<string>('USER_SERVICE_URL');
     const findAllUserUrl = `${userServiceUrl}/users`;
-    const response = await firstValueFrom(
-      this.httpService.get(findAllUserUrl)
-    );
-    return response.data
+    return this.httpService
+      .get(findAllUserUrl)
+      .pipe(map((resp) => resp.data))
+      .pipe(
+        catchError((error: AxiosError) => {
+          const errorMessage =
+            error.response?.data || 'An internal error occurred.';
+          throw new InternalServerErrorException(errorMessage);
+        }),
+      );
   }
 
   async findOne(id: number) {
     const userServiceUrl = this.configService.get<string>('USER_SERVICE_URL');
     const findOneUserUrl = `${userServiceUrl}/users/${id}`;
-    const response = await firstValueFrom(
-      this.httpService.get(findOneUserUrl)
-    );
-    return response.data
+    return this.httpService
+      .get(findOneUserUrl)
+      .pipe(map((resp) => resp.data))
+      .pipe(
+        catchError((error: AxiosError) => {
+          const errorMessage =
+            error.response?.data || 'An internal error occurred.';
+          throw new InternalServerErrorException(errorMessage);
+        }),
+      );
   }
 
   async update(id: number, updateUserDto: UpdateUserDto) {
     const userServiceUrl = this.configService.get<string>('USER_SERVICE_URL');
     const updateUserUrl = `${userServiceUrl}/users/${id}`;
-    const response = await firstValueFrom(
-      this.httpService.patch(updateUserUrl, updateUserDto)
-    );
-    return response.data
+    return this.httpService
+      .patch(updateUserUrl, updateUserDto)
+      .pipe(map((resp) => resp.data))
+      .pipe(
+        catchError((error: AxiosError) => {
+          const errorMessage =
+            error.response?.data || 'An internal error occurred.';
+          throw new InternalServerErrorException(errorMessage);
+        }),
+      );
   }
 
   async remove(id: number) {
@@ -54,6 +79,15 @@ export class UsersService {
     const response = await firstValueFrom(
       this.httpService.delete(removeUserUrl)
     );
-    return response.data
+    return this.httpService
+      .delete(removeUserUrl)
+      .pipe(map((resp) => resp.data))
+      .pipe(
+        catchError((error: AxiosError) => {
+          const errorMessage =
+            error.response?.data || 'An internal error occurred.';
+          throw new InternalServerErrorException(errorMessage);
+        }),
+      );
   }
 }
